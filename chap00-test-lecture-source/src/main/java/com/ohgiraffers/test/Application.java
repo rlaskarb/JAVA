1f8b80ea5b99441d06c550cc540d617eea4caa85
rlaskarb <rlaskarb20@gmail.com>
2024-12-28 23:56:34 +0900

    12/28

--- chap00-test-lecture-source/src/main/java/com/ohgiraffers/test/Application.java
+++ chap00-test-lecture-source/src/main/java/com/ohgiraffers/test/Application.java
@@ -2,19 +2,6 @@ package com.ohgiraffers.test;
 
 public class Application {
 
-    // 한 줄 주석
-    // 필기를 해보아요
-
-    // 여러 줄 주석
-    /*여러
-    * 줄
-    * 주석
-    **/
-    /* 수업목표.  */
-    /* fixme.  */
-    //daads
-    // dasdad
-
     /* 필기.
     *   main 영역은 프로그램을 동작시킬 수 있는 영역이다.
     *  */
@@ -22,7 +9,6 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("안녕하세요");
-//        System.out.println(안녕하세요);
     }
 
 }
